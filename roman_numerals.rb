7f794089a395577b106d1cbd6728e3cc59047906
Nigel Patterson <nigel-patterson@outlook.com>
2020-05-28 19:08:36 -0300

    moved some functions to modules, still not sure how its the best method of organization

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -21,48 +21,13 @@
 #   4. [OK] Sum up all the numbers at the end
 #   5. [OK] Return the Arabic sum number
 
-# Different numeral writing systems (roman/arabic)
-class Numeral
-  SYMBOLS_MAP = { I: 1, V: 5, X: 10, L: 50,
-                  C: 100, D: 500, M: 1000 }.freeze
-
-  def initialize(numeral)
-    @numeral = numeral
-  end
-
-  def to_arabic
-    num_parts = split_numeral(@numeral)
-    num_parts = symbolize(num_parts)
-    valid_num(num_parts).sum
-  end
-
-  def to_roman
-    @numeral = @numeral.to_s if @numeral.is_a? Integer
-    digits = split_numeral(@numeral)
-    digits = places(digits.reverse)
-  end
-
-  def split_numeral(number)
-    num_parts = []
-    number.chars do |ind|
-      num_parts << ind
-    end
-    num_parts
-  end
-
-  def symbolize(num_parts)
+# Functions that help convert arabic to roman format
+module RomanTools
+  def symbolize(num_parts, dict)
     sep = []
     num_parts.each do |char|
       sym = char.to_sym
-      sep << SYMBOLS_MAP[sym]
-    end
-    sep
-  end
-
-  def places(num_parts)
-    sep = []
-    num_parts.each_with_index do |dig, pow|
-      sep << dig.to_i * 10**pow
+      sep << dict[sym]
     end
     sep
   end
@@ -83,6 +48,92 @@ class Numeral
   end
 end
 
+# Functions that end up converting roman to arabic format
+module ArabicTools
+  def places(num_parts)
+    sep = []
+    num_parts.each_with_index do |dig, pow|
+      sep << dig.to_i * 10**pow
+    end
+    sep
+  end
+
+  def thousands(thou)
+    rep = thou / 1000
+    "M" * rep
+  end
+
+  def hundreds(hund)
+    # handle hundreds place
+    # allowed interactions: M = 1000, D = 500, C = 100
+  end
+
+  def tens(ten)
+    # handle the tens place
+    # allowed interactions: C = 100, L = 50, X = 10
+  end
+
+  def ones(unit)
+    # handle the ones/units place
+    # allowed interactions: X V I
+  end
+
+  def delegator(num_parts)
+    ans = []
+    num_parts.each do |num|
+      if num >= 1000
+        ans << thousands(num)
+      elsif num >= 100
+        ans << hundreds(num)
+      elsif num >= 10
+        ans << tens(num)
+      elsif num >= 1
+        ans << ones(num)
+      end
+    end
+    ans
+  end
+
+  def constructor(num)
+    # a function that figures out how to build the roman numerall
+  end
+end
+
+# Different numeral writing systems (roman/arabic)
+class Numeral
+  include RomanTools
+  include ArabicTools
+
+  SYMBOLS_MAP = { I: 1, V: 5, X: 10, L: 50,
+                  C: 100, D: 500, M: 1000 }.freeze
+
+  def initialize(numeral)
+    @numeral = numeral
+  end
+
+  def to_arabic
+    num_parts = split_numeral(@numeral)
+    num_parts = symbolize(num_parts, SYMBOLS_MAP)
+    valid_num(num_parts).sum
+  end
+
+  def to_roman
+    @numeral = @numeral.to_s if @numeral.is_a? Integer
+    digits = split_numeral(@numeral)
+    digits = places(digits.reverse)
+    digits = delegator(digits)
+    digits.join
+  end
+
+  def split_numeral(number)
+    num_parts = []
+    number.chars do |ind|
+      num_parts << ind
+    end
+    num_parts
+  end
+end
+
 # new method to subtract and combine elements of an Array
 class Array
   def subtract_and_comb!(left, right)
@@ -91,8 +142,8 @@ class Array
   end
 end
 
-roman_num = Numeral.new('MMMMMMMMDCCXLVII')
-p roman_num.to_arabic # => 8747
+# roman_num = Numeral.new('MMMMMMMMDCCXLVII')
+# p roman_num.to_arabic # => 8747
 
-arab_num = Numeral.new('123')
+arab_num = Numeral.new('4000')
 p arab_num.to_roman # => [3, 20, 100]
